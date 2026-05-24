36540dbf2d727287c5b708fe664bf9fb05f2f969
FranzCh <franz.chandi@gmail.com>
2026-05-24 10:25:25 -0500

    ui: remove redundant table mode label

--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -905,15 +905,6 @@ const conciseList = (items, limit = 3) => {
       
       <div class="header-right">
         <div class="table-theme-controls">
-          <div class="table-theme-static" title="Map style is available in Map view">
-            <svg class="layer-icon" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
-              <polygon points="12 2 2 7 12 12 22 7 12 2"/>
-              <polyline points="2 17 12 22 22 17"/>
-              <polyline points="2 12 12 17 22 12"/>
-            </svg>
-            <span>{{ themeStore.isDarkMode ? 'Dark' : 'Light' }}</span>
-          </div>
-
           <div class="table-theme-dropdown">
             <button
               type="button"
@@ -1817,7 +1808,6 @@ const conciseList = (items, limit = 3) => {
   gap: 8px;
 }
 
-.table-theme-static,
 .table-theme-trigger,
 .table-mode-toggle {
   display: flex;
@@ -1845,7 +1835,6 @@ const conciseList = (items, limit = 3) => {
   border-color: var(--color-border-light, #4d4d6c);
 }
 
-.table-theme-static .layer-icon,
 .table-theme-trigger .layer-icon,
 .table-mode-toggle .mode-icon {
   width: 16px;
